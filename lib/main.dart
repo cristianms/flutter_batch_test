507e9e023ace553b8229bcc7f1a4259c2eda00f0
Cristian de Mellos <cristian.ms@hotmail.com>
2021-10-28 13:54:10 -0300

    Protótipo funcionando

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_batch_test/pages/my_home_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_batch_test/helpers/database_helper.dart';
+import 'package:flutter_batch_test/models/contato.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -8,6 +10,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   String textoLog = '';
+  int idAutoIncrement = 1;
+  final qtdTeste = 10000;
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 padding: EdgeInsets.only(left: 8, right: 8),
                 child: ElevatedButton(
                   onPressed: _insereNormal,
-                  child: Text('Insere 1000 normal'),
+                  child: Text('Insere normal'),
                 ),
               ),
               Container(
@@ -42,7 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 padding: EdgeInsets.only(left: 8, right: 8),
                 child: ElevatedButton(
                   onPressed: _insereBatch,
-                  child: Text('Insere 1000 batch'),
+                  child: Text('Insere via batch'),
                 ),
               ),
               // Container de logs
@@ -61,21 +65,67 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  void _limpaTabela() {
+  void _limpaTabela() async {
+    final db = await DatabaseHelper().db;
+    await db?.delete('Contato');
+    idAutoIncrement = 1;
     setState(() {
-      textoLog = 'aaa';
+      textoLog = 'A tabela foi limpa\n\n';
     });
   }
 
-  void _insereNormal() {
+  void _insereNormal() async {
+    // Inicia contagem de tempo
+    final stopwatch = Stopwatch()..start();
+    try {
+      // Obtém conexão
+      final db = await DatabaseHelper().db;
+      // Inicia transaction
+      await db?.transaction((txn) async {
+        // Realiza diversas inserções
+        for (var i = 1; i <= qtdTeste; i++) {
+          await txn.insert('Contato', Contato(id: idAutoIncrement++, cpf: idAutoIncrement.toString(), nome: 'Teste $i', tipo: 'normal').toMap());
+        }
+      });
+    } catch (e) {
+      setState(() {
+        textoLog += 'Ocorreu algum erro, verifique...\n\n';
+        print(e.toString());
+      });
+    }
+    // Exibe tempo da execução
     setState(() {
-      textoLog = 'bbb';
+      textoLog += 'Executado normal em ${stopwatch.elapsed}..\n\n';
     });
+    stopwatch.stop();
   }
 
-  void _insereBatch() {
+  void _insereBatch() async {
+    // Inicia contagem de tempo
+    final stopwatch = Stopwatch()..start();
+    try {
+      // Obtém conexão
+      final db = await DatabaseHelper().db;
+      // Inicia transaction
+      await db?.transaction((txn) async {
+        // Inicia batch
+        final batch = txn.batch();
+        // Realiza diversas inserções
+        for (var i = 1; i <= qtdTeste; i++) {
+          batch.insert('Contato', Contato(id: idAutoIncrement++, cpf: i.toString(), nome: 'Teste $i', tipo: 'batch').toMap());
+        }
+        await batch.commit();
+      });
+    } catch (e) {
+      setState(() {
+        textoLog += 'Ocorreu algum erro, verifique...\n\n';
+        print(e.toString());
+      });
+    }
+    // Exibe tempo da execução
     setState(() {
-      textoLog = 'ccc';
+      textoLog += 'Executado via batch em ${stopwatch.elapsed}..\n\n';
     });
+    stopwatch.stop();
   }
 }
